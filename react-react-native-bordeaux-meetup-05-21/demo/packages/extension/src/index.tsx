8f7934d75384968633308cfea04490c2dc440ad1
Stéphane <contact@stephane-richin.fr>
2021-05-26 11:42:42 +0200

    :bug: :troll:

--- react-react-native-bordeaux-meetup-05-21/demo/packages/extension/src/index.tsx
+++ react-react-native-bordeaux-meetup-05-21/demo/packages/extension/src/index.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { AppRegistry } from 'react-native'
 import { MoviesScreen } from '@my-app/ui/src/MoviesScreen'
 
-const Test = () => (
+const App = () => (
   <div style={{ width: 460, height: 200 }}>
     <MoviesScreen />
   </div>
 )
 
-AppRegistry.registerComponent('my-app', () => Test)
+AppRegistry.registerComponent('my-app', () => App)
 AppRegistry.runApplication('my-app', {
   rootTag: document.getElementById('root'),
 })
